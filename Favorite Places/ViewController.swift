57a5b0b9d20a859c3f0fa71553ae630f31c7500c
Sean Perez <seanperez@Seans-MBP-2.attlocal.net>
2016-05-03 13:08:56 -0700

    First commit

---
+++ Favorite Places/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Favorite Places
+//
+//  Created by Sean Perez on 4/24/16.
+//  Copyright © 2016 Awsome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var moreBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        moreBtn.layer.cornerRadius = 2.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
